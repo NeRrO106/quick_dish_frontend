b8f65c60d5372bab863ac024925568a811449e6e
NeRrO106 <andreilimit@yahoo.com>
2025-08-02 18:24:11 +0300

    Updated the register page and creating users in database from frontend

--- src/features/auth/Register.tsx
+++ src/features/auth/Register.tsx
@@ -1,10 +1,53 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Register() {
+  const navigate = useNavigate();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const [error, setError] = useState("");
+
+  const handleRegister = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!form.name || !form.email || !form.password || !form.confirmPassword) {
+      setError("Please fill in all the fields");
+      return;
+    }
+    if (form.password !== form.confirmPassword) {
+      setError("Password doesnt match");
+      return;
+    }
+    if (!form.email.includes("@")) {
+      setError("Invalide email");
+      return;
+    }
+
+    fetch("https://localhost:7100/api/user/createuser", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    })
+      .then((response) => {
+        if (!response.ok) throw new Error("Failed to add new user");
+      })
+      .catch((error) => {
+        console.log("Error adding user: ", error);
+      });
+    navigate("/login");
+  };
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="w-full max-w-md p-8 bg-white rounded-lg shadow-md">
         <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">
           Register
         </h2>
+        {error && <p className="text-black-500">{error}</p>}
         <form>
           <div className="mb-4">
             <label
@@ -18,6 +61,8 @@ function Register() {
               id="username"
               className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Enter your username"
+              onChange={(e) => setForm({ ...form, name: e.target.value })}
+              value={form.name}
             />
           </div>
           <div className="mb-4">
@@ -32,6 +77,8 @@ function Register() {
               id="email"
               className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Enter your email"
+              onChange={(e) => setForm({ ...form, email: e.target.value })}
+              value={form.email}
             />
           </div>
           <div className="mb-6">
@@ -46,6 +93,8 @@ function Register() {
               id="password"
               className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Enter your password"
+              onChange={(e) => setForm({ ...form, password: e.target.value })}
+              value={form.password}
             />
           </div>
           <div className="mb-6">
@@ -57,14 +106,19 @@ function Register() {
             </label>
             <input
               type="password"
-              id="password"
+              id="cpassword"
               className="w-full px-3 py-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-blue-500"
               placeholder="Confirm your password"
+              onChange={(e) =>
+                setForm({ ...form, confirmPassword: e.target.value })
+              }
+              value={form.confirmPassword}
             />
           </div>
           <button
             type="submit"
             className="w-full px-4 py-2 text-white bg-blue-600 rounded hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
+            onClick={(e) => handleRegister(e)}
           >
             Register
           </button>
