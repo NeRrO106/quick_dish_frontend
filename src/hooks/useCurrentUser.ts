c8ffad3bada64a251bf357e1914ed23fde4424be
NeRrO106 <andreilimit@yahoo.com>
2025-09-13 12:00:12 +0300

    Updated hooks components

--- src/hooks/useCurrentUser.ts
+++ src/hooks/useCurrentUser.ts
@@ -2,14 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import type { User } from "../features/users/User";
 
 export function useCurrentUser() {
+  const stored = localStorage.getItem("user");
+  const initialUser = stored ? JSON.parse(stored) : null;
+
   return useQuery<User | null>({
     queryKey: ["currentUser"],
-    queryFn: async () => {
-      const stored = localStorage.getItem("user");
-      if (stored) {
-        return JSON.parse(stored);
-      }
-      return null;
-    },
+    queryFn: () => initialUser,
+    initialData: initialUser,
   });
 }
