6c77e28bf91786ea0cfc438b0d7ece8c6a0145b9
NeRrO106 <andreilimit@yahoo.com>
2026-06-27 12:22:08 +0300

    UI Update

--- src/components/Loading.tsx
+++ src/components/Loading.tsx
@@ -4,7 +4,7 @@ interface LoadingProps {
   message?: string;
 }
 
-const Loading: React.FC<LoadingProps> = ({ message = "Loading..." }) => {
+const Loading: React.FC<LoadingProps> = ({ message = "Se Încarcă..." }) => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-[var(--color-secondary)]">
       <div className="w-12 h-12 border-4 border-t-[var(--color-primary)] border-gray-200 rounded-full animate-spin mb-4"></div>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -11,19 +11,19 @@ const navLinks: Record<Role, { name: string; path: string }[]> = {
   Client: [],
   Guest: [],
   Admin: [
-    { name: "Users", path: "/users" },
-    { name: "Products", path: "/products" },
-    { name: "Orders", path: "/orders" },
-    { name: "Menu", path: "/menu" },
+    { name: "Utilizatori", path: "/users" },
+    { name: "Produse", path: "/products" },
+    { name: "Comenzi", path: "/orders" },
+    { name: "Meniu", path: "/menu" },
   ],
   Manager: [
-    { name: "Products", path: "/products" },
-    { name: "Orders", path: "/orders" },
-    { name: "Menu", path: "/menu" },
+    { name: "Produse", path: "/products" },
+    { name: "Comenzi", path: "/orders" },
+    { name: "Meniu", path: "/menu" },
   ],
   Courier: [
-    { name: "Orders", path: "/orders" },
-    { name: "Menu", path: "/menu" },
+    { name: "Comenzi", path: "/orders" },
+    { name: "Meniu", path: "/menu" },
   ],
 };
 
@@ -115,7 +115,7 @@ function NavBar() {
                         className="block px-4 py-2 text-md text-[var(--text-light)] hover:bg-[var(--color-accent1)]"
                         onClick={() => setIsDropdownOpen(false)}
                       >
-                        My Orders
+                        Comenzi Mele
                       </Link>
                     </li>
                     <li>
@@ -146,7 +146,7 @@ function NavBar() {
               >
                 <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2" />
               </svg>
-              Cart
+              Coș
             </button>
           )}
           {isGuest && (
